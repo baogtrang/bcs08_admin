167fad4f98aa8339f11f83e5e856d9fa86a7306d
quangsi <quangsidev@gmail.com>
2023-09-30 11:43:27 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserPage from "./page/UserPage/UserPage";
+import MoviePage from "./page/MoviePage/MoviePage";
+import HomeLayout from "./layout/Layout";
 
 function App() {
-  return <div className="text-red-400">admin</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<HomeLayout />}>
+            <Route path="/user" element={<UserPage />} />
+            <Route path="/movie" element={<MoviePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/layout/Layout.js
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Outlet } from "react-router-dom";
+const { Header, Content, Sider } = Layout;
+const items1 = ["1", "2", "3"].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  }
+);
+const HomeLayout = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <span className="text-white font-medium text-2xl">
+          Cyber Flix Admin
+        </span>
+        <button className="text-black bg-white rounded px-5 h-10 leading-10 shadow shadow-white">
+          Log in
+        </button>
+      </Header>
+      <Layout>
+        <Sider
+          theme="dark"
+          width={200}
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            style={{
+              height: "100%",
+              borderRight: 0,
+            }}
+            items={items2}
+          />
+        </Sider>
+        <Layout
+          style={{
+            padding: "0 24px 24px",
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: "16px 0",
+            }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>HomeLayout</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            <Outlet />
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+};
+export default HomeLayout;
